3faa7f3310625b5d0be2c31cb8b86dcb6caed3f6
z5488485 <z5488485@vx05>
2023-09-07 22:53:44 +1000

    Added support for vertical whitespace and specific horizontal whitespace scenarios

--- generator.py
+++ generator.py
@@ -2,63 +2,109 @@ import re
 from typing import List, Tuple
 from collections import defaultdict
 
+import lexer
+
+tab_len = 8
+
 def generate(tokens: List[List[Tuple[str, str]]]) -> List[str]:
     # For now, the order of horizontal -> vertical is important.
     tokens = add_horizontal_whitespace(tokens) 
     tokens = add_vertical_whitespace(tokens)
     return tokens_to_text(tokens)
 
-def add_horizontal_whitespace(
-        tokens: List[List[Tuple[str, str]]]
-    ) -> List[List[Tuple[str, str]]]:
-    tab_length = 8
+def add_horizontal_whitespace(tokens: List[List[Tuple[str, str]]]) -> List[List[Tuple[str, str]]]:
     new_tokens = []
-    for i, subtokens in enumerate(tokens):
+    # Do this first
+    for subtokens in tokens:
         new_subtokens = []
         prev_token_type = None
         prev_token_value = None
         prev_prev_token_type = None
         for token_type, token_value in subtokens:
             if token_type == 'INSTRUCTION':
-                new_subtokens.append(('WHITESPACE', ' ' * tab_length))
-            if (prev_token_type == 'COMMA' or 
-                prev_token_type == 'LABEL_DEFINITION' and not token_type == 'DIRECTIVE' or 
-                prev_token_type == 'DIRECTIVE' and not prev_prev_token_type == 'LABEL_DEFINITION' or
-                not prev_token_type == None and token_type == 'COMMENT'):
-                new_subtokens.append(('WHITESPACE', ' '))
+                new_subtokens.append(('WHITESPACE', ' ' * tab_len))
             if prev_token_type == 'INSTRUCTION':
-                new_subtokens.append(('WHITESPACE', ' ' * (tab_length - len(prev_token_value))))
-            if token_type == 'DIRECTIVE' and prev_token_type == 'LABEL_DEFINITION':
-                new_subtokens.append(('WHITESPACE', ' ' * (tab_length - len(prev_token_value))))
-            if prev_prev_token_type == 'LABEL_DEFINITION' and prev_token_type == 'DIRECTIVE':
-                new_subtokens.append(('WHITESPACE', ' ' * (tab_length - len(prev_token_value))))
+                new_subtokens.append(('WHITESPACE', ' ' * (tab_len - len(prev_token_value))))
+            if prev_token_type == 'COMMA' or prev_token_type == 'LABEL_DEFINITION' and not token_type == 'DIRECTIVE' or prev_token_type == 'DIRECTIVE' and not prev_prev_token_type == 'LABEL_DEFINITION' or prev_token_type == 'INSTRUCTION':
+                new_subtokens.append(('WHITESPACE', ' '))
+
             new_subtokens.append((token_type, token_value))
             prev_prev_token_type = prev_token_type
             prev_token_type = token_type
             prev_token_value = token_value
         new_tokens.append(new_subtokens)
-        if (i < len(tokens) - 1 and
-            subtokens[0][0] == 'COMMENT' and
-            len(subtokens) == 1 and
-            not tokens[i + 1][0][0] == 'LABEL_DEFINITION'):
-            new_tokens[i].insert(0, ('WHITESPACE', ' ' * tab_length))
-    longest_line = 0;
+
+    new_tokens = align_right_side_comments(new_tokens)
+    new_tokens = align_data_section(new_tokens)
+    new_tokens = align_inline_comments(new_tokens)
+    return new_tokens
+
+def align_data_section(tokens):
+    return align_data_comments(align_data_code(tokens))
+
+def align_data_code(tokens):
+    new_tokens = tokens
+    longest_len = 0
     for subtokens in new_tokens:
-        line_length = 0
-        for token_type, token_value in subtokens:
-            line_length += len(token_value) if not token_type == 'COMMENT' else 0
-        longest_line = line_length if line_length > longest_line else longest_line
-    smallest_multiplier = 0
-    while longest_line > tab_length * smallest_multiplier:
-        smallest_multiplier += 1
+        if len(subtokens) < 2 or not (subtokens[0][0] == 'LABEL_DEFINITION' and subtokens[1][0] == 'DIRECTIVE'):
+            continue
+        longest_len = max(longest_len, len(subtokens[0][1]))
+
+    multiplier = 0
+    while longest_len > tab_len * multiplier:
+        multiplier += 1
+
     for i, subtokens in enumerate(tokens):
-        if (subtokens[len(subtokens) - 1][0] == 'COMMENT' and
-            len(new_tokens[i]) > 1 and
-            new_tokens[i][1][0] == 'INSTRUCTION'):
-            length = 0
-            for _, token_value in new_tokens[i][:-1]:
-                length += len(token_value)
-            new_tokens[i].insert(len(new_tokens[i]) - 1, ('WHITESPACE', ' ' * (smallest_multiplier * tab_length - length)))
+        if len(subtokens) < 2 or not (subtokens[0][0] == 'LABEL_DEFINITION' and subtokens[1][0] == 'DIRECTIVE'):
+            continue
+        new_tokens[i].insert(1, ('WHITESPACE', ' ' * (multiplier * tab_len - len(subtokens[0][1]))))
+        new_tokens[i].insert(3, ('WHITESPACE', ' ' * ((multiplier + 1) * tab_len - len(subtokens[0][1]) - len(subtokens[1][1]) - len(subtokens[2][1]))))
+    return new_tokens
+
+def align_data_comments(tokens):
+    new_tokens = tokens
+    longest_len = 0
+    for subtokens in new_tokens:
+        if not lexer.label_directive_regex.match(tokens_to_line(subtokens)):
+            continue
+        longest_len = max(longest_len, sum(len(token_value) for _, token_value in subtokens[:-1]))
+
+    multiplier = 0
+    while longest_len > tab_len * multiplier:
+        multiplier += 1
+
+    for i, subtokens in enumerate(tokens):
+        if not lexer.label_directive_regex.match(tokens_to_line(subtokens)):
+            continue
+        new_tokens[i].insert(len(subtokens) - 1, ('WHITESPACE', ' ' * (tab_len * multiplier - sum(len(token_value) for _, token_value in subtokens[:-1]))))
+    return new_tokens
+
+def align_inline_comments(tokens):
+    new_tokens = tokens
+    for i, subtokens in enumerate(tokens):
+        if not (lexer.comment_regex.match(tokens_to_line(subtokens)) and i + 1 < len(tokens) and not lexer.code_label_definition_regex.match(tokens_to_line(tokens[i + 1]))):
+            continue
+        new_tokens[i].insert(0, ('WHITESPACE', ' ' * tab_len))
+    return new_tokens
+
+def align_right_side_comments(tokens):
+    new_tokens = tokens
+    longest_len = 0
+    for subtokens in tokens:
+        if not (len(subtokens) > 1 and subtokens[1][0] == 'INSTRUCTION' and subtokens[len(subtokens) - 1][0] == 'COMMENT'):
+            continue
+        line_len = 1 + sum(len(token_value) for _, token_value in subtokens[:-1])
+        longest_len = max(longest_len, line_len)
+
+    multiplier = 0
+    while longest_len > tab_len * multiplier:
+        multiplier += 1
+    
+    for i, subtokens in enumerate(tokens):
+        if not (len(subtokens) > 1 and subtokens[1][0] == 'INSTRUCTION' and subtokens[len(subtokens) - 1][0] == 'COMMENT'):
+            continue
+        line_len = sum(len(token_value) for _, token_value in subtokens[:-1])
+        new_tokens[i].insert(len(subtokens[:-1]), ('WHITESPACE', ' ' * (multiplier * tab_len - line_len)))
     return new_tokens
 
 def add_vertical_whitespace(
@@ -98,3 +144,12 @@ def tokens_to_text(tokens: List[List[Tuple[str, str]]]) -> List[str]:
             line += token_value
         lines.append(line)
     return lines
+
+def tokens_to_line(tokens):
+    line = ''
+    for _, token_value in tokens:
+        line += token_value
+    return line
+
+    
+    
