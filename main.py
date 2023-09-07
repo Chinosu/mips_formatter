e9f72824f292a3ef2e877282b004e3a7a1a2cac1
z5488485 <z5488485@vx05>
2023-09-07 22:54:49 +1000

    Replaced debug code with CLI interface.

--- main.py
+++ main.py
@@ -1,63 +1,29 @@
+import sys
+import os
+
 import lexer
 import generator
 
-test_code = '''.data                   # section for data segment
-arr: .word 64, 34, 25, 12, 22, 11, 90
-size: .word 7
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <filename.s> or <filename>.asm")
+    sys.exit(1)
 
-.text
-.globl main
+file_name = sys.argv[1]
 
-main:  # main function start
-    la $t0, arr         # Load base address of array into $t0
-    lw $t1, size        # Load size of array into $t1
+if not os.path.exists(file_name):
+    print(f"Error: File {file_name} not found.")
+    sys.exit(1)
 
-    # Outer loop
-    outer_loop:
-        li $t2, 0       # Initialize inner loop counter to 0
-        li $t3, 1       # Initialize swapped flag to true (1)
-        # Inner loop
-        inner_loop:
-            beqz $t3, outer_end   # If no two elements were swapped by inner loop, then break
-            li $t3, 0             # Reset swapped flag to false (0)
-            # Inner loop logic
-            inner_logic:
-                beq $t2, $t1, inner_end  # If inner loop counter reaches array size, exit inner loop
-                lw $t4, 0($t0)           # Load arr[i] into $t4
-                lw $t5, 4($t0)           # Load arr[i + 1] into $t5
-                ble $t4, $t5, skip_swap  # If arr[i] <= arr[i + 1], skip swap
-                # Swap arr[i] and arr[i + 1]
-                sw $t5, 0($t0)
-                sw $t4, 4($t0)
-                li $t3, 1               # Set swapped flag to true (1)
-                skip_swap:
-                addi $t0, $t0, 4        # Move to the next element in the array
-                addi $t2, $t2, 1        # Increment inner loop counter
-                j inner_logic
+if not (file_name.endswith('.s') or file_name.endswith('.asm')):
+    print("Error: PLease provide a .s or .asm file.")
+    sys.exit(1)
 
-            inner_end:
-            # Reset array pointer and inner loop counter
-            la $t0, arr
-            li $t2, 0
-            # Decrement outer loop counter (size)
-            sub $t1, $t1, 1
-            j outer_loop
+formatted_code = ''
+with open(file_name, 'r') as file:
+    unformatted_code = file.read()
+    tokens = lexer.lex(unformatted_code)
+    formatted_code = generator.generate(tokens)
 
-        outer_end:
-        # End of program
-        li $v0, 10          # exit syscall
-        syscall
-
-'''
-
-tokens = lexer.lex(test_code)
-# for i, subtokens in enumerate(tokens):
-    # # print(f'Tokens for line {i + 1}: {subtokens}')
-
-token_count = lexer.count_tokens(tokens)
-# for token_type in token_count.keys():
-    # print(f'There are {token_count[token_type]} counts of token type {token_type}')
-
-code = generator.generate(tokens)
-for line in code:
-    print(line)
+with open(file_name, 'w') as file:
+    for line in formatted_code:
+        file.write(line + '\n')
