0d68f7ba3b3e45db0026d45bd5f5a4f7829576fb
z5488485 <z5488485@vx05>
2023-09-09 17:42:30 +1000

    Added error output when encountering UNKNOWN tokens

--- main.py
+++ main.py
@@ -18,12 +18,16 @@ if not (file_name.endswith('.s') or file_name.endswith('.asm')):
     print("Error: PLease provide a .s or .asm file.")
     sys.exit(1)
 
-formatted_code = ''
+tokens = None
 with open(file_name, 'r') as file:
     unformatted_code = file.read()
     tokens = lexer.lex(unformatted_code)
-    formatted_code = generator.generate(tokens)
+
+if lexer.count_tokens(tokens)['UNKNOWN'] != 0:
+    print("Error: UNKNOWN tokens encountered.")
+    sys.exit(1)
 
 with open(file_name, 'w') as file:
+    formatted_code = generator.generate(tokens)
     for line in formatted_code:
         file.write(line + '\n')
